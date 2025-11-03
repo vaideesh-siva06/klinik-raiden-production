9fa3ff3dd30e2bc3a018154003b6ea505faebc84
Vaideesh-siv <vaideesh.siv@gmail.com>
2025-11-02 20:52:33 -0600

    About page

--- app/components/AnimatedComponent.tsx
+++ app/components/AnimatedComponent.tsx
@@ -9,22 +9,22 @@ interface AnimatedProp {
 
 export default function AnimatedComponent({ phrases, delay }: AnimatedProp) {
   return (
-    <button className="relative text-3xl md:text-4xl leading-relaxed hover:cursor-pointer">
-    {phrases.map((phrase, index) => (
+    <div className="relative text-3xl md:text-4xl leading-relaxed cursor-text">
+      {phrases.map((phrase, index) => (
         <motion.span
-        key={index}
-        className="block"
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-            delay: delay | index * 1, // ⏱ delay between each phrase
-            duration: delay | 1.2,
+          key={index}
+          className="block cursor-text"
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: delay + index * 1, // delay between each phrase
+            duration: 1.2,
             ease: 'easeOut',
-        }}
+          }}
         >
-        {phrase}
+          {phrase}
         </motion.span>
-    ))}
-    </button>
+      ))}
+    </div>
   )
 }
