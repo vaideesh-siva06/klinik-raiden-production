ed24517e0b46408b7357ab8794218d44f2b92c9a
Vaideesh-siv <vaideesh.siv@gmail.com>
2025-11-04 09:11:19 -0600

    NextConfig updated image implementation

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**', // allow all paths from Cloudinary
+      },
+    ],
   },
 
   env: {
     ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
   },
 };
 
